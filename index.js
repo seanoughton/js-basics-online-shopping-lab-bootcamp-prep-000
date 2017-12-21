7ef14a58236f6662802e491b155edb2d6d6a5e2b
Sean Oughton <seanoughton@gmail.com>
2017-12-21 16:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(item) {
   var itemName = item 
   var itemPrice = Math.random() * (100 - 1) + 1;
   itemPrice = Math.floor(itemPrice)
-  var cartItem = new Object({ itemName: itemPrice })
+  var cartItem = new Object({ [itemName]: itemPrice })
 
 }
 
