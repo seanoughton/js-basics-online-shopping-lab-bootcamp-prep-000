cbbdf7cbba7f94317b547af3d029c9a36e1d2d9d
Sean Oughton <seanoughton@gmail.com>
2017-12-21 17:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   } else {
       var i = 0
       while (i<cart.length){
-          console.log(`In your cart, you have ${itemPrice} at `)
+          console.log(`In your cart, you have ${itemName} at ${itemPrice`)
           // itemName at itemPrice, and 
           // terminate sentence with a period
       i += 1
