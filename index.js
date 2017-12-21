eb955df19fb40d58c4e3e6a9a28c37e849491ba4
Sean Oughton <seanoughton@gmail.com>
2017-12-21 17:11:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  newString = `In your cart, you have`
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.")
   } else {
